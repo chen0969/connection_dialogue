23a595b62612188c537654901d991e302508bfc6
Canada-Broccoli <chen0969@algonquinlive.com>
2024-08-16 12:18:43 -0400

    fix hand location

--- js/effects.js
+++ js/effects.js
@@ -1,6 +1,14 @@
 $(window).on('scroll', function() {
     var scrollTop = $(this).scrollTop();
-    var newTop = 150 + scrollTop;
+    var isMobile = window.matchMedia("(max-width: 768px)").matches; // Adjust the width for your mobile breakpoint
+
+    var newTop;
+    if (isMobile) {
+        newTop = scrollTop;
+    } else {
+        newTop = 150 + scrollTop;
+    }
+
     if(newTop <= 2000){
         $(".c-hand").css("transform", "translateY(-" + newTop +"px)");
     }else{
